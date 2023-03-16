5ebca951c299bf972a689d4ab8349316eeca971f
SamuelLauro <samuellauro0612@gmail.com>
2023-03-16 20:18:53 -0300

    adicionei cores diferentes

--- cursoJS/aula12 ex/ex015/script.js
+++ cursoJS/aula12 ex/ex015/script.js
@@ -16,30 +16,38 @@ function verificar(){
             if(idade >= 0 && idade <= 10){
                         // CRIANÇA
                 img.setAttribute('src','imagens/bebe.m.png')
+                document.body.style.background = '#668200'
             }else if(idade > 10 && idade <= 20){
                         // JOVEM
                 img.setAttribute('src','imagens/jovem.m.png')
+                document.body.style.background = '#8C91B1'
             }else if(idade < 60){
                         // ADULTO
                 img.setAttribute('src','imagens/adulto.m.png')
+                document.body.style.background = '#73729E'
             }else{
                 // IDOSO
                 img.setAttribute('src','imagens/idoso.m.png')
+                document.body.style.background ='#699C7D'
             }
         } else if (fsex[1].checked){
             genero = 'Mulher';
             if(idade >= 0 && idade <= 10){
                          // CRIANÇA
                 img.setAttribute('src','imagens/bebe.f.png')
+                document.body.style.background = '#9796B7'
                 }else if(idade > 10 && idade <= 20){
                          // JOVEM
                 img.setAttribute('src','imagens/jovem.f.png')
+                document.body.style.background = '#EA958E'
                 }else if(idade < 60){
                            // ADULTO
                 img.setAttribute('src','imagens/adulto.f.png')
+                document.body.style.background = '#9796B7'
                 }else{
                          // IDOSO
                 img.setAttribute('src','imagens/idoso.f.png')
+                document.body.style.background ='#DDB867'
                 }
         }
         
