bc3cbc6f923363c6dd8a0c157af11686acde009b
SamuelLauro <samuellauro0612@gmail.com>
2023-04-28 18:54:26 -0300

    treinando

---
+++ curso JS cont/index.js
@@ -0,0 +1,9 @@
+var item1 = "arroz";
+var item2 = "feijão";
+var item3 = "macarrão";
+var item4 = "leite";
+
+const pessoa = ["Dimitri", "Teixeira", 30, "Brasileiro"]
+
+document.getElementById('teste').innerHTML = pessoa.join(' - ');
+
